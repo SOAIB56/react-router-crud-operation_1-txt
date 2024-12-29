983c3e8c99830576ea74ea72b8583b1217ac3100
Soaib Ahmed <129555727+SOAIB56@users.noreply.github.com>
2024-12-29 13:51:47 +0600

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,40 @@
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+
+//   </StrictMode>,
+// )
+
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import ErrorPage from "./ErrorPage";
+import "./index.css";
+import Contact from "./routes/Contact";
+import Root from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
